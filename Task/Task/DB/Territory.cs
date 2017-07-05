4c878825807e2ea4022f26381c3887f43916cf9d
Ilya.Myalik <Ilya.Myalik@thomsonreuters.com>
2017-07-05 19:28:23 +0300

    Entities refactoring

--- Task/Task/DB/Territory.cs
+++ Task/Task/DB/Territory.cs
@@ -1,11 +1,8 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace Task.DB
 {
-    using System;
-    using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-    using System.ComponentModel.DataAnnotations.Schema;
-    using System.Data.Entity.Spatial;
-
     public partial class Territory
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
